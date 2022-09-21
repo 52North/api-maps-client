ca4b76b47d005c913dac0be0055f7a9cab94552c
Prakhar Gurunani <prakhargurunani@gmail.com>
2022-09-21 22:25:11 +0530

    WIP

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ogcmaps",
-    version="0.1.0",
+    version="0.1.1",
     author="52°North",
     author_email="info@52north.org",
     description="Python client and wrapper for OGC Maps API",
